37b43fb0b39b84653c9b2b1fda6cdb75aa3bab7b
Ignacio R Espino <irespino@capozios.com>
2018-07-17 20:18:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(arg)
+  if arg % 3 == 0 || arg % 5 == 0
+    
 end
