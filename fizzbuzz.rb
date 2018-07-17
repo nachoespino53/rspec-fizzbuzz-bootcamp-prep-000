2ce793f4e06f59d0217958ea14228d4a69ca2f7a
Ignacio R Espino <irespino@capozios.com>
2018-07-17 20:17:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(arg)
-  if arg % 3 == 0 || arg % 5 == 0
-    return "FizzBuzz"
-  end
-  elsif arg % 3 == 0
-    return "Fizz"
-  end
-  elsif arg & 5 == 0
-  return "Buzz"
-  end
-  else 
-  return nil
-end
+def fizzbuzz
 end
