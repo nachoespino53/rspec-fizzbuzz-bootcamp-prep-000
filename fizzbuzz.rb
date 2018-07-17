c7ddd8b7b031b4bab137fc565c29b06dd2859ab0
Ignacio R Espino <irespino@capozios.com>
2018-07-17 20:20:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,11 @@
 
 def fizzbuzz(arg)
   if arg % 3 == 0 || arg % 5 == 0
-    
+    return "FizzBuzz"
+  elsif arg % 3 == 0
+    return "Fizz"
+  elsif arg % 5 == 0
+    return "Buzz"
+  else
+    return nil
 end
